2693e8b438f11c3a8c1611a8d19a3703b2ffd93c
Belal Uddin <139705476+coderBelal@users.noreply.github.com>
2024-12-10 20:17:47 +0600

    Update Product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -97,7 +97,7 @@ const Product = () => {
       </p>
     </div>
 
-    <div className="grid grid-cols-2 gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
       {products.map((product) => (
         <div
           key={product.id}
